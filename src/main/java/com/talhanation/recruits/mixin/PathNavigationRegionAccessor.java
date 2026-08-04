c16d06d6083e56525faece54f6819ec4b0ba51cd
Scypterodaptyl <scypterodaptyl@gmail.com>
2026-08-04 19:07:53 +0300

    Add accessor for PathNavigationRegion's underlying level

---
+++ src/main/java/com/talhanation/recruits/mixin/PathNavigationRegionAccessor.java
@@ -0,0 +1,12 @@
+package com.talhanation.recruits.mixin;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.PathNavigationRegion;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(PathNavigationRegion.class)
+public interface PathNavigationRegionAccessor {
+    @Accessor("level")
+    Level talhanation$getLevel();
+}
